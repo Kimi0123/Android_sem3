a020fb5b7381f42bd6dd74327e0c711c4811c7ff
Kimi0123 <maharjannirvan01@gmail.com>
2025-07-30 15:35:35 +0545

    Update Product

---
+++ app/src/main/java/com/example/ai36/model/ProductModel.kt
@@ -0,0 +1,9 @@
+package com.example.ai36.model
+
+data class ProductModel(
+    var productId : String = " ",
+    var productName : String = " ",
+    var productPrice : Double = 0.0,
+    var productDescription : String = " ",
+    var image  : String = "",
+)
